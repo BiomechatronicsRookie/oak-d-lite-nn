e58dc77b8b14494831e948178f839d401c9769bb
Víctor Carmona <victor.carmona.ortiz@gmail.com>
2024-01-25 01:01:19 +0100

    Refactored code to select and show stream based on stream mode

--- classes/camera/camera.py
+++ classes/camera/camera.py
@@ -9,6 +9,7 @@ import time
 import os
 import warnings
 
+## TODO: Add config file for automatic setting of stream features
 class Camera():
     def __init__(self):
         self.rgbResolution = dai.ColorCameraProperties.SensorResolution.THE_1080_P
@@ -42,25 +43,40 @@ class Camera():
         XOutRgb.setStreamName("rgb")
         self.rbg_cam.video.link(XOutRgb.input)
 
-        # Create 
+        # Create Mono camera and config
         self.mono = self.pipeline.createMonoCamera()
         self.mono.setResolution(self.monoResolution)
         self.mono.setFps(float(self.mono_fps))
-        self.mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        self.mono.setBoardSocket(dai.CameraBoardSocket.CAM_B)
             # Create XLinkOut
         XOutMono = self.pipeline.createXLinkOut()
         XOutMono.setStreamName("mono")
         self.mono.out.link(XOutMono.input)
 
-    def stream(self):
 
-        display_res = ((int(self.rbg_cam.getVideoWidth()/2), int(self.rbg_cam.getVideoHeight()/2)))
+        # Create Mono camera right and config
+        self.mono_right = self.pipeline.createMonoCamera()
+        self.mono_right.setResolution(self.monoResolution)
+        self.mono_right.setFps(float(self.mono_fps))
+        self.mono_right.setBoardSocket(dai.CameraBoardSocket.CAM_C)
+            # Create XLinkOut
+        XOutMono_right = self.pipeline.createXLinkOut()
+        XOutMono_right.setStreamName("mono_right")
+        self.mono_right.out.link(XOutMono_right.input)
+
+    def stream(self, mode ='rgb'):
+        if mode == 'rgb':
+            display_res = ((int(self.rbg_cam.getVideoWidth()/2), int(self.rbg_cam.getVideoHeight()/2)))
+        elif mode == 'mono':
+            display_res = ((int(self.mono.getResolutionWidth()/2), int(self.mono.getResolutionHeight()/2)))
+        elif mode == 'mono_right':
+            display_res = ((int(self.mono_right.getResolutionWidth()/2), int(self.mono_right.getResolutionHeight()/2)))
 
         while True:
-            queueName = self.device.getQueueEvent("mono")
+            queueName = self.device.getQueueEvent("{0}".format(mode))
             message = self.device.getOutputQueue(queueName, maxSize=1, blocking = False).get()
-            self.curr_rgb_frame = message.getCvFrame()
-            im_res = cv2.resize(self.curr_rgb_frame.copy(), display_res)
+            self.curr_frame = message.getCvFrame()
+            im_res = cv2.resize(self.curr_frame.copy(), display_res)
             cv2.imshow("img", im_res)
             # Use interactive keys from opencv
             val = cv2.waitKey(1)
@@ -69,238 +85,68 @@ class Camera():
                 cv2.destroyAllWindows()
                 self.device.close()
                 break
-
         return
     
-    
-    
-    def streamRgb(self, calibrate = False):
-        # Create pipeline
-        pipeline = dai.Pipeline()
+   
+    def streamWithBoardPose(self, mode = 'rgb'):
+        
+        calibData = self.device.readCalibration()
 
-        # Create Color Camera node and set RES and FPS
-        cam = pipeline.createColorCamera()
-        cam.setResolution(self.rgbResolution)
-        cam.setVideoSize(1920,1080)
-        cam.setFps(float(self.rgb_fps))
-        cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-
-        # Set link with pc
-        cam.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-        xout_rgb = pipeline.createXLinkOut()
-        xout_rgb.setStreamName("RGB")
-        cam.video.link(xout_rgb.input)
-
-        fps = False
-        self.recording_rgb = False
-        display_res = ((int(cam.getVideoWidth()/2), int(cam.getVideoHeight()/2)))
-
-        with dai.Device(pipeline) as device:
-            while True:
-                t1 = time.monotonic()
-                queueName = device.getQueueEvent("RGB")
-                t = time.monotonic() - t1
-                message = device.getOutputQueue(queueName, maxSize=1, blocking = False).get()
-                self.curr_rgb_frame = message.getCvFrame()
-                if self.recording_rgb:
-                    output.write(self.curr_rgb_frame)
-                if t > 0 and fps:
-                    cv2.putText(self.curr_rgb_frame, "FPS: {0}".format(1/t), (100, 100), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 0), 1, cv2.LINE_AA, False) 
-                im_res = cv2.resize(self.curr_rgb_frame.copy(), display_res)
-                cv2.imshow("img", im_res)
-                           
-                # Use interactive keys from opencv
-                val = cv2.waitKey(1)
-                # Close windows and stop streaming
-                if val in [ord('q'), ord('Q')]:
-                    if self.recording_rgb:
-                        self.recording_rgb = False
-                        output.release()
-                    cv2.destroyAllWindows()
-                    device.close()
-                    break
-                
-                # If in calibration mode store images in the image buffer
-                elif val == ord(' ') and calibrate:
-                    self.calibration_buffer.append(self.curr_rgb_frame.copy())
-                    print('Img {0} of 15'.format(len(self.calibration_buffer)))
-                    if len(self.calibration_buffer) == 15:
-                        cv2.destroyAllWindows()
-                        device.close()
-                        break
-
-                # In any mode, toggle printing fps on the image
-                elif val in [ord('f'), ord('F')]:
-                    fps = not(fps)
-                
-                elif val in [ord('r'), ord('R')]:
-                    if self.recording_rgb:
-                        self.recording_rgb = False
-                        output.release() 
-                    
-                    else:
-                        if self.recording_rgb == False:
-                            video_dir = os.getcwd() + '\\data\\videos\\'
-                            video_name = '{0}.mp4'.format(str(len(os.listdir(video_dir))).zfill(4))
-                            output = cv2.VideoWriter(video_dir + video_name, cv2.VideoWriter_fourcc(*'mp4v'), self.rgb_fps, cam.getVideoSize())
-                            self.recording_rgb = True  
-        return
-
-    def streamRgbWithBoardPose(self):
-        # Create pipeline
-        pipeline = dai.Pipeline()
-
-        # Create Color Camera node and set RES and FPS
-        cam = pipeline.createColorCamera()
-        cam.setResolution(self.rgbResolution)
-        cam.setVideoSize(1920,1080)
-        cam.setFps(float(self.rgb_fps))
-        cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-
-        # Set link with pc
-        cam.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-        xout_rgb = pipeline.createXLinkOut()
-        xout_rgb.setStreamName("RGB")
-        cam.video.link(xout_rgb.input)
-
-        fps = False
-        display_res = ((int(cam.getVideoWidth()/2), int(cam.getVideoHeight()/2)))
-
-        with dai.Device(pipeline) as device:
-            calibData = device.readCalibration()
+        if mode == 'rgb':
+            display_res = ((int(self.rbg_cam.getVideoWidth()/2), int(self.rbg_cam.getVideoHeight()/2)))
             self.mtx = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_A, 1920, 1080)).squeeze()
             self.dst = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_A))
-            # Define board parameters
-            dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
-            board = cv2.aruco.CharucoBoard((7,5),.025,.0125,dictionary)
+        elif mode == 'mono':
+            display_res = ((int(self.mono.getResolutionWidth()/2), int(self.mono.getResolutionHeight()/2)))
+            self.mtx = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, 640, 480)).squeeze()
+            self.dst = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_B))
+        elif mode == 'mono_right':
+            display_res = ((int(self.mono_right.getResolutionWidth()/2), int(self.mono_right.getResolutionHeight()/2)))
+            self.mtx = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_C, 640, 480)).squeeze()
+            self.dst = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.CAM_C))
+        
+        # Define board parameters
+        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
+        board = cv2.aruco.CharucoBoard((7,5),.025,.0125,dictionary)
 
-            while True:
-                t1 = time.monotonic()
-                queueName = device.getQueueEvent("RGB")
-                t = time.monotonic() - t1
-                message = device.getOutputQueue(queueName, maxSize=1, blocking = False).get()
-                self.curr_rgb_frame = message.getCvFrame()
-                rot, tr = self.EstimateBoardPose(self.curr_rgb_frame, board, dictionary)
-
-                if t > 0 and fps:
-                    cv2.putText(self.curr_rgb_frame, "FPS: {0}".format(1/t), (100, 100), cv2.FONT_HERSHEY_SIMPLEX , 1, (255, 0, 0), 1, cv2.LINE_AA, False)
-                cv2.drawFrameAxes(self.curr_rgb_frame, self.mtx, self.dst, rot, tr, 0.1)
-                im_res = cv2.resize(self.curr_rgb_frame.copy(), display_res)
-                cv2.imshow("img", im_res)
-                           
-                # Use interactive keys from opencv
-                val = cv2.waitKey(1)
-                # Close windows and stop streaming
-                if val in [ord('q'), ord('Q')]:
-                    cv2.destroyAllWindows()
-                    device.close()
-                    break
-
-                # In any mode, toggle printing fps on the image
-                elif val in [ord('f'), ord('F')]:
-                    fps = not(fps)
+        while True:
+            queueName = self.device.getQueueEvent("{0}".format(mode))
+            message = self.device.getOutputQueue(queueName, maxSize=1, blocking = False).get()
+            self.curr_frame = message.getCvFrame()
+            rot, tr = self.EstimateBoardPose(self.curr_frame, board, self.mtx, self.dst, dictionary)
+            
+            cv2.drawFrameAxes(self.curr_frame, self.mtx, self.dst, rot, tr, 0.1)
+            im_res = cv2.resize(self.curr_frame.copy(), display_res)
+            cv2.imshow("img", im_res)
+                        
+            # Use interactive keys from opencv
+            val = cv2.waitKey(1)
+            # Close windows and stop streaming
+            if val in [ord('q'), ord('Q')]:
+                cv2.destroyAllWindows()
+                self.device.close()
+                break
         return
-    
-    
-    def streamMono(self, side = None):
-        # Create pipeline
-        pipeline = dai.Pipeline()
+        
+    def stream2Mono(self):
+        while True:
+            left_queue = self.device.getQueueEvent("mono")
+            right_queue = self.device.getQueueEvent("mono_right")
+            message_l = self.device.getOutputQueue(left_queue, maxSize=1, blocking = False).get()
+            message_r = self.device.getOutputQueue(right_queue, maxSize=1, blocking = False).get()
+            l_frame = message_l.getCvFrame()
+            r_frame = message_r.getCvFrame()
+            self.curr_mono_frame = (l_frame/2 + r_frame/2)/255
 
-        # Create Color Camera node and set RES and FPS
-        mono = pipeline.createMonoCamera()
-        mono.setResolution(self.monoResolution)
-        mono.setFps(float(self.mono_fps))
+            cv2.imshow("img", self.curr_mono_frame)
+            val = cv2.waitKey(1)
+            if val in [ord('q'), ord('Q')]:
+                cv2.destroyAllWindows()
+                self.device.close()
+                break
 
-        # Set link with pc
-        if side == 'LEFT' or side == None:
-            mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
-            if not side:
-                warnings.warn("No camera was selected. Using mono LEFT camera...")
-        if side == 'RIGHT':
-            mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-
-        xout_mono = pipeline.createXLinkOut()
-        xout_mono.setStreamName("mono")
-        mono.out.link(xout_mono.input)
-
-        fps = False
-
-        with dai.Device(pipeline) as device:
-            while True:
-                t1 = time.monotonic()
-                queueName = device.getQueueEvent("mono")
-                t = time.monotonic() - t1
-                message = device.getOutputQueue(queueName, maxSize=1, blocking = False).get()
-                self.curr_mono_frame = message.getCvFrame()
-                if t > 0 and fps:
-                    cv2.putText(self.curr_mono_frame, "FPS: {0}".format(int(1/t)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 0), 1, cv2.LINE_AA, False) 
-                cv2.imshow("img", self.curr_mono_frame)
-                val = cv2.waitKey(1)
-                if val in [ord('q'), ord('Q')]:
-                    cv2.destroyAllWindows()
-                    device.close()
-                    break
-                elif val in [ord('f'), ord('F')]:
-                    fps = not(fps)
-        return
-    
-    def stream2Mono(self, calibrate = False):
-        # Create pipeline
-        pipeline = dai.Pipeline()
-
-        # Create Color Camera node and set RES and FPS
-        mono_r = pipeline.createMonoCamera()
-        mono_r.setResolution(self.monoResolution)
-        mono_r.setFps(float(self.mono_fps))
-        mono_l = pipeline.createMonoCamera()
-        mono_l.setResolution(self.monoResolution)
-        mono_l.setFps(float(self.mono_fps))
-
-        # Set link with pc
-        mono_r.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-        mono_l.setBoardSocket(dai.CameraBoardSocket.LEFT)
-
-        xout_mono_r = pipeline.createXLinkOut()
-        xout_mono_r.setStreamName("mono_r")
-        xout_mono_l = pipeline.createXLinkOut()
-        xout_mono_l.setStreamName("mono_l")
-        mono_r.out.link(xout_mono_r.input)
-        mono_l.out.link(xout_mono_l.input)
-
-        fps = False
-
-        with dai.Device(pipeline) as device:
-            while True:
-                t1 = time.monotonic()
-                left_queue = device.getQueueEvent("mono_l")
-                right_queue = device.getQueueEvent("mono_r")
-                t = time.monotonic() - t1
-                message_l = device.getOutputQueue(left_queue, maxSize=1, blocking = False).get()
-                message_r = device.getOutputQueue(right_queue, maxSize=1, blocking = False).get()
-                l_frame = message_l.getCvFrame()
-                r_frame = message_r.getCvFrame()
-                self.curr_mono_frame = (l_frame/2 + r_frame/2)/255
-
-                if t > 0 and fps:
-                    cv2.putText(self.curr_mono_frame, "FPS: {0}".format(int(1/t)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 0), 1, cv2.LINE_AA, False) 
-                cv2.imshow("img", self.curr_mono_frame)
-                val = cv2.waitKey(1)
-                if val in [ord('q'), ord('Q')]:
-                    cv2.destroyAllWindows()
-                    device.close()
-                    break
-
-                elif val in [ord('f'), ord('F')]:
-                    fps = not(fps)
-
-                elif val == ord(' ') and calibrate:
-                    self.calibration_buffer_left.append(l_frame.copy())
-                    self.calibration_buffer_right.append(r_frame.copy())
-                    print('Imgs {0} of 15'.format(len(self.calibration_buffer_left)))
-                    if len(self.calibration_buffer_left) == 15:
-                        cv2.destroyAllWindows()
-                        device.close()
-                        break
+            elif val in [ord('f'), ord('F')]:
+                fps = not(fps)
         return
 
     def streamDisparity(self):
@@ -355,74 +201,7 @@ class Camera():
                     device.close()
                     break
         return
-    
-    
-    def calibrateOakRgb(self): # Intended to be called when for sure calibration is wanted
-        cal_path = os.path.dirname(os.path.realpath(__file__))              # Get subdirectory for the camera class to store params there
-        self.calibration_buffer = []
-
-        self.streamRgb(True)       # Stream for calibration
-
-        mtx, dst = self._runCalibration(cal_path)
-        # Override calibration parameters inside the camera:
-        pipeline = dai.Pipeline()
-
-        # Create Color Camera node and set RES and FPS
-        cam = pipeline.createColorCamera()
-        with dai.Device(pipeline) as device:
-            calibData = device.readCalibration()
-            calibData.setCameraIntrinsics(dai.CameraBoardSocket.CAM_A, mtx.tolist(), 1920, 1080)
-            calibData.setDistortionCoefficients(dai.CameraBoardSocket.CAM_A, dst.ravel().tolist())
-            device.close()
-
-        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
-        board = cv2.aruco.CharucoBoard((7,5),.025,.0125,dictionary)
-        
-        for img in self.calibration_buffer:
-            r, t = self.EstimateBoardPose(img, board, self.mtx, self.dst, dictionary)
-            cv2.drawFrameAxes(img, self.mtx, self.dst, r, t, 0.1)
-            cv2.imshow("img", img)
-            cv2.waitKey(0)
-
-        cv2.destroyAllWindows()
-
-        return
-    
-    def calibrateOak2Mono(self): # Intended to be called when for sure calibration is wanted
-        cal_path = os.path.dirname(os.path.realpath(__file__))              # Get subdirectory for the camera class to store params there
-        self.calibration_buffer_left = []
-        self.calibration_buffer_right = []
-        buff = [self.calibration_buffer_left, self.calibration_buffer_right]
-        sockets = [dai.CameraBoardSocket.CAM_B, dai.CameraBoardSocket.CAM_C]        # Left, Right
-
-        self.stream2Mono(True)       # Stream for calibration
-
-        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
-        board = cv2.aruco.CharucoBoard((7,5),.025,.0125,dictionary)
-
-        for i in range(0,2):
-            self.calibration_buffer = buff[i]
-            mtx, dst = self._runCalibration(cal_path)
-            # Override calibration parameters inside the camera:
-            pipeline = dai.Pipeline()
-
-            with dai.Device(pipeline) as device:
-                calibData = device.readCalibration()
-                calibData.setCameraIntrinsics(sockets[i], mtx.tolist(), 640, 480)
-                calibData.setDistortionCoefficients(sockets[i], dst.ravel().tolist())
-                device.close()
-
-            for img in self.calibration_buffer:
-                r, t = self.EstimateBoardPose(img, board, mtx, dst, dictionary)
-                cv2.drawFrameAxes(img, mtx, dst, r, t, 0.1)
-                cv2.imshow("img", img)
-                cv2.waitKey(0)
-
-        cv2.destroyAllWindows()
-        
-        return
-    
-    
+       
     ''' CALIBRATION FUNCTION HELPERS FOR THE OAK D CAMERA'''
     
     def _runCalibration(self, cal_path):
@@ -536,4 +315,4 @@ class Camera():
     
 if __name__ == '__main__':
         oak = Camera()
-        oak.stream()
+        oak.streamWithBoardPose('mono_right')
